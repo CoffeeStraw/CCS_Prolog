b4c31ba7a8cd74e9318160c96c9db6af88bc515e
CoffeeStraw <clarantonio98@gmail.com>
2021-05-17 18:34:09 +0200

    Fixed LaTeX conversions for variables and actions containing underscores

--- src/utils.pl
+++ src/utils.pl
@@ -21,11 +21,14 @@ derivation_to_tex(infer(Name, Conclusion, P), Latex) :- derivation_to_tex(P, P_L
                                                                             P_Latex, '}\n'], Latex).
 
 % Write expressions in LaTeX using package `proof`
-expr_to_tex(in_delta(V, E), Latex) :- expr_to_tex(E, E_Latex),
-                                      atomic_list_concat([V, ' \\triangleq ', E_Latex, ' \\in ', '\\Delta'], Latex).
+expr_to_tex(in_delta(V, E), Latex) :- escape_atom(V, V_Latex),
+                                      expr_to_tex(E, E_Latex),
+                                      atomic_list_concat([V_Latex, ' \\triangleq ', E_Latex, ' \\in ', '\\Delta'], Latex).
 
-expr_to_tex(notin_set(A, S), Latex) :- atomic_list_concat(S, ', ', S_Latex),
-                                       atomic_list_concat([A, ' \\notin ', '\\{', S_Latex, '\\}'], Latex).
+expr_to_tex(notin_set(A, S), Latex) :- escape_atom(A, A_Latex),
+                                       maplist(escape_atom, S, S_Mapped),
+                                       atomic_list_concat(S_Mapped, ', ', S_Latex),
+                                       atomic_list_concat([A_Latex, ' \\notin ', '\\{', S_Latex, '\\}'], Latex).
 
 expr_to_tex(red(E1, A, E2), Latex) :- expr_to_tex(E1, E1_Latex),
                                       expr_to_tex(A, A_Latex),
@@ -45,12 +48,13 @@ expr_to_tex(pre(E1, E2), Latex) :- expr_to_tex(E1, E1_Latex),
                                    atomic_list_concat([E1_Latex, '.', E2_Latex], Latex).
 
 expr_to_tex(res(E, S), Latex) :- expr_to_tex(E, E_Latex),
-                                 atomic_list_concat(S, ', ', S_Latex),
+                                 maplist(escape_atom, S, S_Mapped),
+                                 atomic_list_concat(S_Mapped, ', ', S_Latex),
                                  atomic_list_concat([E_Latex, ' \\; \\symbol{92} \\{', S_Latex, '\\}'], Latex).
 
 expr_to_tex(rel(E, S), Latex) :- expr_to_tex(E, E_Latex),
-                                 maplist(expr_to_tex, S, S_mapped),
-                                 atomic_list_concat(S_mapped, ', ', S_Latex),
+                                 maplist(expr_to_tex, S, S_Mapped),
+                                 atomic_list_concat(S_Mapped, ', ', S_Latex),
                                  atomic_list_concat([E_Latex, '\\;[', S_Latex, ']'], Latex).
 
 expr_to_tex(p(E), Latex) :- expr_to_tex(E, E_Latex),
@@ -58,14 +62,18 @@ expr_to_tex(p(E), Latex) :- expr_to_tex(E, E_Latex),
 
 expr_to_tex(nil, '\\textbf{nil}').
 expr_to_tex(tau, '\\tau').
-expr_to_tex(var(V), V).
-expr_to_tex(in(A), A).
-expr_to_tex(out(A), Latex) :- atomic_list_concat(['\\overline{', A, '}'], Latex).
-expr_to_tex(pair(A1, A2), Latex) :- atomic_list_concat([A1, '/', A2], Latex).
+expr_to_tex(var(V), Latex)       :- escape_atom(V, Latex).
+expr_to_tex(in(A), Latex)        :- escape_atom(A, Latex).
+expr_to_tex(out(A), Latex)       :- escape_atom(A, A_Latex), atomic_list_concat(['\\overline{', A_Latex, '}'], Latex).
+expr_to_tex(pair(A1, A2), Latex) :- escape_atom(A1, A1_Latex), escape_atom(A2, A2_Latex),
+                                    atomic_list_concat([A1_Latex, ' / ', A2_Latex], Latex). % Note: 'pair' is used in relabelling
+
+% Using re_replace twice is an HACK, since using `re_replace('_'/ag, '\\_', X, X_Latex)` does not work
+escape_atom(X, Latex) :- re_replace('_'/ag, '\\!', X, TMP), re_replace('!'/ag, '_', TMP, Escaped), atomic_list_concat(['\\text{', Escaped, '}'], Latex).
 
 % Write LaTeX content to file
 write_to_tex(Content, Path) :- open(Path, write, Out),
-                               write(Out, '\\documentclass[10pt]{article}\n\\usepackage{proof}\n\\usepackage{amsmath}\n\\usepackage{amssymb}\n\n\\begin{document}\n\\[\n'),
+                               write(Out, '\\documentclass[fleqn,10pt]{article}\n\\usepackage{proof}\n\\usepackage{amsmath}\n\\usepackage{amssymb}\n\\usepackage[paperwidth=20in, margin=0.5in]{geometry}\n\n\\begin{document}\n\\[\n'),
                                write(Out, Content),
                                write(Out, '\\]\n\\end{document}'),
                                close(Out).
